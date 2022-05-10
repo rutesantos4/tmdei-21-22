6318bb5ea4aa10ac66372a9adce60d6dd9b5c85e
RuteSantos <1160663@isep.ipp.pt>
2022-05-10 18:32:35 +0100

    CryptocurrencyPaymentAPI - Unit tests of RestClientException and ServiceUnavailableException

---
+++ UnitTests/CryptocurrencyPaymentAPI.Validations/Exceptions/RestClientExceptionTests.cs
@@ -0,0 +1,66 @@
+﻿namespace UnitTests.CryptocurrencyPaymentAPI.Validations.Exceptions
+{
+    using AutoFixture;
+    using FluentAssertions;
+    using global::CryptocurrencyPaymentAPI.Validations.Exceptions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RestClientExceptionTests
+    {
+        private readonly IFixture fixture;
+
+        public RestClientExceptionTests()
+        {
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void OnGetMessage_GivenAString_ShouldReturnValidMessage()
+        {
+            // Arrange
+            var message = fixture.Create<string>();
+            var status = fixture.Create<int>();
+            var reason = fixture.Create<string>();
+            var exception = new RestClientException(message, status, reason);
+
+            // Act
+            var result = exception.Message;
+
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestMethod]
+        public void OnGetStatus_GivenAnInt_ShouldReturnValidMessage()
+        {
+            // Arrange
+            var message = fixture.Create<string>();
+            var status = fixture.Create<int>();
+            var reason = fixture.Create<string>();
+            var exception = new RestClientException(message, status, reason);
+
+            // Act
+            var result = exception.Status;
+
+            // Assert
+            result.Should().Be(status);
+        }
+
+        [TestMethod]
+        public void OnGetReason_GivenAString_ShouldReturnValidMessage()
+        {
+            // Arrange
+            var message = fixture.Create<string>();
+            var status = fixture.Create<int>();
+            var reason = fixture.Create<string>();
+            var exception = new RestClientException(message, status, reason);
+
+            // Act
+            var result = exception.Reason;
+
+            // Assert
+            result.Should().Be(reason);
+        }
+    }
+}
---
+++ UnitTests/CryptocurrencyPaymentAPI.Validations/Exceptions/ServiceUnavailableExceptionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace UnitTests.CryptocurrencyPaymentAPI.Validations.Exceptions
+{
+    using AutoFixture;
+    using FluentAssertions;
+    using global::CryptocurrencyPaymentAPI.Validations.Exceptions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ServiceUnavailableExceptionTests
+    {
+        private readonly IFixture fixture;
+
+        public ServiceUnavailableExceptionTests()
+        {
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void OnGetMessage_GivenAString_ShouldReturnValidMessage()
+        {
+            // Arrange
+            var errorMessage = fixture.Create<string>();
+            var exception = new ServiceUnavailableException(errorMessage);
+
+            // Act
+            var result = exception.Message;
+
+            // Assert
+            result.Should().Be($"Invalid operation. None of payment gateways is {errorMessage} to process transaction. Please retry later.");
+        }
+    }
+}
