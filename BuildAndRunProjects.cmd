2c9d08d693685fbd1fb43e0eee105d680c27395c
RuteSantos <1160663@isep.ipp.pt>
2022-06-05 21:41:00 +0100

    Script to run all the projects

---
+++ BuildAndRunProjects.cmd
@@ -0,0 +1,7 @@
+start /d "%~dp0\CryptocurrencyPaymentAPI"       dotnet watch run CryptocurrencyPaymentAPI
+start /d "%~dp0\CryptocurrencyPaymentAuth"      dotnet watch run CryptocurrencyPaymentAuth
+start /d "%~dp0\CryptocurrencyPaymentConfiguration" dotnet watch run CryptocurrencyPaymentConfiguration
+start /d "%~dp0\CryptocurrencyPaymentGateways" dotnet watch run CryptocurrencyPaymentGateways
+
+
+PAUSE
